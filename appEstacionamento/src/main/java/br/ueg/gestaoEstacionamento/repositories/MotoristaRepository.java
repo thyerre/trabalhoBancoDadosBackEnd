8193fee22db921a827365a467c80fc21113b7e68
renansousa1999 <renansb20181@gmail.com>
2019-06-13 21:21:48 -0300

    foi adicionado a classe GaragemController

--- appEstacionamento/src/main/java/br/ueg/gestaoEstacionamento/repositories/MotoristaRepository.java
+++ appEstacionamento/src/main/java/br/ueg/gestaoEstacionamento/repositories/MotoristaRepository.java
@@ -1,9 +1,14 @@
 package br.ueg.gestaoEstacionamento.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.ueg.gestaoEstacionamento.model.Motorista;
 
 public interface MotoristaRepository extends CrudRepository<Motorista, Long>{
+	
+
 
 }
