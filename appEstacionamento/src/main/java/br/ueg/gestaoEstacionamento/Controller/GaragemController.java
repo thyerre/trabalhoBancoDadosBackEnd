710d1cc36f8715e675f31eec5f8b79b01e547059
thyerre Rangel <thyerre321@gmail.com>
2019-06-17 19:06:56 -0300

    corrigindo garagem controle

--- appEstacionamento/src/main/java/br/ueg/gestaoEstacionamento/Controller/GaragemController.java
+++ appEstacionamento/src/main/java/br/ueg/gestaoEstacionamento/Controller/GaragemController.java
@@ -2,6 +2,7 @@ package br.ueg.gestaoEstacionamento.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ueg.gestaoEstacionamento.model.Garagem;
 import br.ueg.gestaoEstacionamento.model.Login;
 import br.ueg.gestaoEstacionamento.repositories.GaragemRepository;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("v1/Garagem")
 public class GaragemController {
@@ -22,38 +23,36 @@ public class GaragemController {
 		private GaragemRepository GaragemRepository;
 		
 		@GetMapping
-		public ResponseEntity<?>listAll()
+		public ResponseEntity<?>listAllGaragem()
 		{
 			return new ResponseEntity<>(GaragemRepository.findAll(),HttpStatus.OK);
 		}
-		@GetMapping(path = "{/id_garagem}")
-		public ResponseEntity<?> getLoginById(@PathVariable("id_garagem") Long id_garagem)
-		{
-			return new ResponseEntity(this.GaragemRepository.findById(id_garagem), HttpStatus.OK);
+//		@GetMapping(path = "{/id_garagem}")
+//		public ResponseEntity<?> getGaragemById(@PathVariable("id_garagem") Long id_garagem)
+//		{
+//			return new ResponseEntity(this.GaragemRepository.findById(id_garagem), HttpStatus.OK);
 //			Login login = loginRepository.findById(id_login);
 //			if (login == null)
 //				return new ResponseEntity<>("login not found", HttpStatus.NOT_FOUND);
 //			return new ResponseEntity<>(login,HttpStatus.OK);
-		}
-		@PostMapping
-		public ResponseEntity<?> save(@RequestBody Garagem Garagem) 
-		{
-	 		return new ResponseEntity<>(GaragemRepository.save(Garagem), HttpStatus.OK);
-		}
-		@DeleteMapping(path = "/{id_garagem}")
-		public ResponseEntity<Garagem> deleteEntity (@PathVariable long id_garagem){
-
-			this.GaragemRepository.deleteById(id_garagem);
-			return new ResponseEntity(HttpStatus.OK);
-		}
-		
-		@PutMapping("/{id_garagem}")
-		public ResponseEntity<?>UpdateVeiculo(@RequestBody Garagem Garagem)
-		{
-			this.GaragemRepository.save(Garagem);
-			return new ResponseEntity<>(HttpStatus.OK);
-		}
-		
-		
-		
+//		}
+//		@PostMapping
+//		public ResponseEntity<?> saveGaragem(@RequestBody Garagem Garagem) 
+//		{
+//	 		return new ResponseEntity<>(GaragemRepository.save(Garagem), HttpStatus.OK);
+//		}
+//		@DeleteMapping(path = "/{id_garagem}")
+//		public ResponseEntity<Garagem> deleteGaragem (@PathVariable long id_garagem){
+//
+//			this.GaragemRepository.deleteById(id_garagem);
+//			return new ResponseEntity(HttpStatus.OK);
+//		}
+//		
+//		@PutMapping("/{id_garagem}")
+//		public ResponseEntity<?>UpdateGaragem(@RequestBody Garagem Garagem)
+//		{
+//			this.GaragemRepository.save(Garagem);
+//			return new ResponseEntity<>(HttpStatus.OK);
+//		}
+				
 }
